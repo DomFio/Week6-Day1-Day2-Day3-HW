6cc3d721524d4216a46b0bb816f96dc65c8ef98f
Dominic Fiorelli <dominicfiorelli0223@gmail.com>
2022-04-19 20:51:25 -0500

    homework W6D2

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 # GIve access to the project in any OS we find ourself in
 # Allow outside files/folders to be added to the project from
@@ -12,6 +16,8 @@ class Config():
     Using Enviornment variables where available otherwise
     create a config variable if not done already.
     """
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or "You will never guess nanananabooboo"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEPLOY_DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
